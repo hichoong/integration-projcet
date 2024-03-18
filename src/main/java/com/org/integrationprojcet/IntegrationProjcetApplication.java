13f825826d3e524407b23ea6da74659320845e86
choong <ksky9230@naver.com>
2024-03-18 11:39:10 +0900

    Initial commit

---
+++ src/main/java/com/org/integrationprojcet/IntegrationProjcetApplication.java
@@ -0,0 +1,13 @@
+package com.org.integrationprojcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationProjcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationProjcetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/integrationprojcet/IntegrationProjcetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.integrationprojcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationProjcetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
